1cc3ddb52bdb4148bfbf73372701e0112f2b3ed2
leblog92 <50671954+leblog92@users.noreply.github.com>
2026-03-19 16:53:07 +0100

    aaa

---
+++ v2/pip.bat
@@ -0,0 +1,9 @@
+python -m pip install --upgrade pip
+
+# Installer les packages un par un
+pip install opencv-python==4.10.0.84
+pip install Pillow==11.1.0
+pip install pandas==2.2.3
+pip install openpyxl==3.1.5
+pip install piexif==1.1.3
+pip install numpy==2.2.4
---
+++ v2/start_app3.bat
@@ -0,0 +1,5 @@
+:: Lancer l'application
+echo.
+echo Lancement de l'application...
+start /B python app3.py
+pause
